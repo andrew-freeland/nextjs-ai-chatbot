4007cfec15c0e8163f649a7122dee452f0c40ac7
andrew-freeland <andrewcunn23@gmail.com>
2025-04-23 14:33:29 -0700

    Update route.ts

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,6 @@ import { authOptions } from '@/app/(auth)/auth';
 
 const handler = NextAuth(authOptions);
 
+// This is correct for App Router in NextAuth v5+
 export const GET = handler;
 export const POST = handler;

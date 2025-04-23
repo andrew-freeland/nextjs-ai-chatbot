9b0bf414a9a3461fb7fb1f0d48074eb0a433056b
andrew-freeland <andrewcunn23@gmail.com>
2025-04-23 13:18:16 -0700

    Update route.ts

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,10 @@ import NextAuth from 'next-auth';
 import { authOptions } from '@/app/(auth)/auth';
 import { NextRequest } from 'next/server';
 
-// This is now the required signature in v5
-const handler = (req: NextRequest) => {
-  return NextAuth(req, authOptions);
-};
+export async function GET(req: NextRequest) {
+  return await NextAuth(req, authOptions);
+}
 
-export { handler as GET, handler as POST };
+export async function POST(req: NextRequest) {
+  return await NextAuth(req, authOptions);
+}

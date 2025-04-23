4464dd7a2a42808358c14b81a883cd6eb8733021
andrew-freeland <andrewcunn23@gmail.com>
2025-04-23 11:27:41 -0700

    Update route.ts

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,5 @@ import { authOptions } from '@/app/(auth)/auth';
 
 const handler = NextAuth(authOptions);
 
-const handlers = {
-  GET: handler,
-  POST: handler,
-};
-
-export { handlers as GET, handlers as POST };
+export const GET = handler;
+export const POST = handler;

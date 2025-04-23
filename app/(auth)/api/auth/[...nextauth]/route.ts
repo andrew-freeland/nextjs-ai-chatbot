b54e02dcdd936f8f2c53eaefe9f7a1dad2a31685
andrew-freeland <andrewcunn23@gmail.com>
2025-04-23 14:06:40 -0700

    Update route.ts

--- app/(auth)/api/auth/[...nextauth]/route.ts
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,9 @@ import { authOptions } from '@/app/(auth)/auth';
 import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  return await NextAuth(req, authOptions);
+  return await NextAuth(req, authOptions) as Response;
 }
 
 export async function POST(req: NextRequest) {
-  return await NextAuth(req, authOptions);
+  return await NextAuth(req, authOptions) as Response;
 }
